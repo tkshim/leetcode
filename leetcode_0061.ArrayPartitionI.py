78abe41b9515d3338eba218a831920c2d9f7271f
Takeshi Shimmura <tshimmura@tshimmura-ltm2.internal.salesforce.com>
2019-09-18 10:02:58 +0900

    0918

---
+++ leetcode_0061.ArrayPartitionI.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+L = [1,4,3,2,5,6]
+
+
+#1. ソートする
+L.sort()
+
+#2 レングスを確認する
+print len(L)
+
+#3 ２で割ってNを求める。
+N=len(L)/2
+print N
+#4 N分割する
+リストから二個ずつ取り出す。
+01 23 45
+
+#5 それぞれのMINを求める。
+L1=[1,2]
+print 'Min is: ', min(L1)
+
+#6 MINの足し算をする。
