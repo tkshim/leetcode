dbc76f8a1fc5e40f12bcb6d2dae8383d22073672
Takeshi Shimmura <tshimmura@tshimmura-ltm2.internal.salesforce.com>
2019-09-20 16:42:16 +0900

    0920

--- leetcode_0061.ArrayPartitionI.py
+++ leetcode_0061.ArrayPartitionI.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 #coding: utf-8
 
+'''
+n=0
+Z=[]
+while n < N:
+    x = 2 * n
+    Z.append(L[x:x+2])
+    n += 1
+print Z
+'''
+
+def minsum(L):
+    #数字を昇順にソート
+    L.sort()
+    #レングスを２で割って分割数を求める
+    N = len(L)/2
+    n = 0
+    #合計値
+    sum = 0
+    while n < N:
+        #0,2,4,6,8〜の行列を作成
+        x = 2 * n
+        #最小値、つまりindexの偶数の値を合計
+        sum += L[x]
+        n += 1
+    return sum
 L = [1,4,3,2,5,6]
-
-
-#1. ソートする
-L.sort()
-
-#2 レングスを確認する
-print len(L)
-
-#3 ２で割ってNを求める。
-N=len(L)/2
-print N
-#4 N分割する
-リストから二個ずつ取り出す。
-01 23 45
-
-#5 それぞれのMINを求める。
-L1=[1,2]
-print 'Min is: ', min(L1)
-
-#6 MINの足し算をする。
+print minsum(L)
